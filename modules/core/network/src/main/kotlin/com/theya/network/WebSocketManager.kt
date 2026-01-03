300ec08481a1fc810deb735f0ae34c8ad6b8cf58
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:33:46 +0100

    Create WebSocketManager.kt

---
+++ modules/core/network/src/main/kotlin/com/theya/network/WebSocketManager.kt
@@ -0,0 +1,13 @@
+package com.theya.network
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class WebSocketManager @Inject constructor() {
+    fun connect(url: String) {
+    }
+    
+    fun disconnect() {
+    }
+}
