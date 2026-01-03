935c987eed843faa7251b6873ced6bd88dd6f469
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:43:57 +0100

    Create FilePickerViewModel.kt

---
+++ modules/features/bridge-explorer/src/main/kotlin/com/theya/bridgeexplorer/viewmodel/FilePickerViewModel.kt
@@ -0,0 +1,9 @@
+package com.theya.bridgeexplorer.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class FilePickerViewModel @Inject constructor() : ViewModel() {
+    fun processFile(uri: String) {
+    }
+}
