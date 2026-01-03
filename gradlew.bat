77ca00adcd533f514cf1f925128fc6457d81baf6
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 23:15:51 +0100

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+set GRADLE_WRAPPER_JAR=%~dp0gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%GRADLE_WRAPPER_JAR%" (
+  echo ERROR: gradle-wrapper.jar is missing
+  echo Run: gradle wrapper --gradle-version 9.2.1
+  exit /b 1
+)
+
+java -Xmx64m -Xms64m -jar "%GRADLE_WRAPPER_JAR%" %*
