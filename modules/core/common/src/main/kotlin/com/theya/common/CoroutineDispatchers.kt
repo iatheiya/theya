15df2fa35fadd8ab0cd5a0cbe6560f73f6f1d810
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:16:18 +0100

    Create CoroutineDispatchers.kt

---
+++ modules/core/common/src/main/kotlin/com/theya/common/CoroutineDispatchers.kt
@@ -0,0 +1,13 @@
+package com.theya.common
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CoroutineDispatchers @Inject constructor() {
+    val io: CoroutineDispatcher = Dispatchers.IO
+    val main: CoroutineDispatcher = Dispatchers.Main
+    val default: CoroutineDispatcher = Dispatchers.Default
+}
