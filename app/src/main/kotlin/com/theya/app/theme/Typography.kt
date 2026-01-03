f0b9dc27a994c7c0bbcd1ad8ffa2c0adcd95ec07
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:52:04 +0100

    Create Typography.kt

---
+++ app/src/main/kotlin/com/theya/app/theme/Typography.kt
@@ -0,0 +1,24 @@
+package com.theya.app.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
+
+val Purple80 = androidx.compose.ui.graphics.Color(0xFFD0BCFF)
+val PurpleGrey80 = androidx.compose.ui.graphics.Color(0xFFCCC2DC)
+val Pink80 = androidx.compose.ui.graphics.Color(0xFFEFB8C8)
+val Purple40 = androidx.compose.ui.graphics.Color(0xFF6650a4)
+val PurpleGrey40 = androidx.compose.ui.graphics.Color(0xFF625b71)
+val Pink40 = androidx.compose.ui.graphics.Color(0xFF7D5260)
