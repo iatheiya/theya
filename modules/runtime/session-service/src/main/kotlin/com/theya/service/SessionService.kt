1d77572904c5c0326cd498dea18d830c3e691f59
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:46:28 +0100

    Create SessionService.kt

---
+++ modules/runtime/session-service/src/main/kotlin/com/theya/service/SessionService.kt
@@ -0,0 +1,31 @@
+package com.theya.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SessionService : Service() {
+
+    @Inject
+    lateinit var lifecycleController: LifecycleController
+
+    @Inject
+    lateinit var binder: ServiceBinder
+
+    override fun onCreate() {
+        super.onCreate()
+        lifecycleController.startForeground(this)
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        return binder
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycleController.stop()
+    }
+}
