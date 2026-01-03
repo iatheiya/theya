fab3832d5f1fc28b10d5f9dd1519214933c01330
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-03 22:12:07 +0100

    Create build.gradle.kts

---
+++ modules/system/glue-jni/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.mozilla.rust-android-gradle.rust-android")
+}
+
+android {
+    namespace = "com.theya.glue"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+cargo {
+    module = "../engine-rs"
+    libname = "theya_engine"
+    targets = listOf("arm", "arm64", "x86", "x86_64")
+    profile = "release"
+}
+
+dependencies {
+    implementation("net.java.dev.jna:jna:5.13.0")
+}
