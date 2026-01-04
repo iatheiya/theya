1e4e1176ce149a055317ed35524eeb86adf46134
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 20:39:26 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/kotlin/p/dev")
-        maven("https://maven.mozilla.org/")
+        maven("https://raw.githubusercontent.com/mozilla/rust-android-gradle/main/maven")
     }
 }
 
