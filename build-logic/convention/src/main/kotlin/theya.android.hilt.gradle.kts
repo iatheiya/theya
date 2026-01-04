87f0b38f752ea04427d9a1d9a7f680079b2ba746
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 14:03:46 +0100

    Update theya.android.hilt.gradle.kts

--- build-logic/convention/src/main/kotlin/theya.android.hilt.gradle.kts
+++ build-logic/convention/src/main/kotlin/theya.android.hilt.gradle.kts
@@ -1,11 +1,9 @@
-import com.theya.buildlogic.libs
-
 plugins {
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.kapt")
 }
 
 dependencies {
-    "implementation"(libs.hilt.android)
-    "kapt"(libs.hilt.compiler)
+    "implementation"(libs.findLibrary("hilt.android").get())
+    "kapt"(libs.findLibrary("hilt.compiler").get())
 }
