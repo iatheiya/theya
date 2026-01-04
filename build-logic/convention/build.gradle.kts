a8090906ba445ead6a8bff56c8953a2c5dd8e833
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 15:27:32 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,11 +2,19 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    gradlePluginPortal()
+    maven("https://plugins.gradle.org/m2/")
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
 group = "com.theya.buildlogic"
 
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
     implementation(libs.hilt.android.gradlePlugin)
-    implementation(libs.rust.android.gradlePlugin)
+    implementation("org.mozilla.rust-android-gradle:plugin:0.9.6")
 }
