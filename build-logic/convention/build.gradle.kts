2d808c75a49a35d38e348775171c3dec655e487c
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 13:48:38 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -7,4 +7,6 @@ group = "com.theya.buildlogic"
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
+
+implementation(libs.hilt.android.gradlePlugin) 
 }
