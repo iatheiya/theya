5023cb970c06c3d5ae6196c590b2cdd9990bb0b2
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 18:52:17 +0100

    Update settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -4,8 +4,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://plugins.gradle.org/m2/")
-
-maven("https://maven.pkg.jetbrains.space/public/p/kotlin/p/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/kotlin/p/dev")
     }
 }
 
