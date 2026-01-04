33ffc31eb8a42ba72d7987b7917b309b75d12643
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 14:30:17 +0100

    Update theya.android.application.gradle.kts

--- build-logic/convention/src/main/kotlin/theya.android.application.gradle.kts
+++ build-logic/convention/src/main/kotlin/theya.android.application.gradle.kts
@@ -1,34 +1,25 @@
 import com.android.build.api.dsl.ApplicationExtension
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 
-class AndroidApplicationConventionPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        with(target) {
-            with(pluginManager) {
-                apply("com.android.application")
-                apply("org.jetbrains.kotlin.android")
-            }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-            extensions.configure<ApplicationExtension> {
-                compileSdk = 35
-                
-                defaultConfig {
-                    targetSdk = 35
-                    minSdk = 24
-                }
-                
-                buildTypes {
-                    release {
-                        isMinifyEnabled = true
-                        proguardFiles(
-                            getDefaultProguardFile("proguard-android-optimize.txt"),
-                            "proguard-rules.pro"
-                        )
-                    }
-                }
-            }
+extensions.configure<ApplicationExtension> {
+    compileSdk = 35
+    
+    defaultConfig {
+        targetSdk = 35
+        minSdk = 24
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
