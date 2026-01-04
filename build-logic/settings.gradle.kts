d55bcd7c548e51603e1c6da041341d0b2e6880de
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 17:15:19 +0100

    Update settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,7 +1,24 @@
-dependencyResolutionManagement {
+pluginManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+        maven("https://plugins.gradle.org/m2/")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
 }
 
