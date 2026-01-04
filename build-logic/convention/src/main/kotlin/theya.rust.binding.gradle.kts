15dced4960eb25b5e1175f8be64c950b5f1b52bb
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 14:38:23 +0100

    Update theya.rust.binding.gradle.kts

--- build-logic/convention/src/main/kotlin/theya.rust.binding.gradle.kts
+++ build-logic/convention/src/main/kotlin/theya.rust.binding.gradle.kts
@@ -1,12 +1,3 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-
-class RustBindingConventionPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        with(target) {
-            with(pluginManager) {
-                apply("org.mozilla.rust-android-gradle.rust-android")
-            }
-        }
-    }
+plugins {
+    id("org.mozilla.rust-android-gradle.rust-android")
 }
