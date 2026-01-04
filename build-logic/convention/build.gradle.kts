3e2415f0ac2a118bce652f71ca256fdfb2742bd9
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 14:14:39 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -7,6 +7,22 @@ group = "com.theya.buildlogic"
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.hilt.android.gradlePlugin)
+}
 
-implementation(libs.hilt.android.gradlePlugin) 
+gradlePlugin {
+    plugins {
+        register("theyaAndroidApplication") {
+            id = "theya.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("theyaAndroidCompose") {
+            id = "theya.android.compose"
+            implementationClass = "theya.android.compose.gradle"
+        }
+        register("theyaAndroidHilt") {
+            id = "theya.android.hilt"
+            implementationClass = "theya.android.hilt.gradle"
+        }
+    }
 }
