8d57ee166433ffab566fd6bba002fcdea1641779
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 21:53:43 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -8,12 +8,12 @@ repositories {
     google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/kotlin/p/dev")
-    maven("https://mozilla.github.io/rust-android-gradle/maven")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.hilt.android.gradlePlugin)
-    compileOnly("org.mozilla.rust-android-gradle:plugin:0.9.3")
+    compileOnly(libs.rust.android.gradlePlugin)
 }
