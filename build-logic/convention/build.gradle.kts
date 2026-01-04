b8af4512b6c1e7a8694dfd83e7a7b6f8a374199d
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-04 22:06:22 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -7,6 +7,8 @@ group = "com.theya.buildlogic"
 repositories {
     google()
     mavenCentral()
+    gradlePluginPortal()
+    maven("https://plugins.gradle.org/m2/")
     maven("https://maven.pkg.jetbrains.space/public/p/kotlin/p/dev")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
