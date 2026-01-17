5023fccf248a962853d2ec8e66ee724330615b2b
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 15:06:41 +0100

    Create ProjectExtensions.kt

---
+++ build-logic/convention/src/main/kotlin/com/theya/buildlogic/ProjectExtensions.kt
@@ -0,0 +1,8 @@
+package com.theya.buildlogic
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
+val Project.libs: VersionCatalog
+    get() = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
