ca5b6a169946ea633383ac28e1410c9c4103022c
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 15:03:22 +0100

    Update ProjectExtensions.kt

--- build-logic/convention/src/main/kotlin/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/ProjectExtensions.kt
@@ -1,7 +1,8 @@
+package com.theya.buildlogic
+
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.getByType
 
 val Project.libs: VersionCatalog
-    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+    get() = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
