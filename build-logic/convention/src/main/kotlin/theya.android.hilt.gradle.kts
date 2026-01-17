447f09315882a5bca65d2641e5fa3d03383007c2
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 15:15:02 +0100

    Delete build-logic/convention/src/main/kotlin/theya.android.hilt.gradle.kts

--- build-logic/convention/src/main/kotlin/theya.android.hilt.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("com.google.dagger.hilt.android")
-    id("org.jetbrains.kotlin.kapt")
-}
-
-dependencies {
-    "implementation"(libs.findLibrary("hilt.android").get())
-    "kapt"(libs.findLibrary("hilt.compiler").get())
-}
