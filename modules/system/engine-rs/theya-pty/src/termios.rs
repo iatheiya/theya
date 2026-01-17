86678c70b799e9e7f8560f4608d1a1f2dccba0b9
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-18 00:19:26 +0100

    Update termios.rs

--- modules/system/engine-rs/theya-pty/src/termios.rs
+++ modules/system/engine-rs/theya-pty/src/termios.rs
@@ -1,6 +1,7 @@
 use nix::sys::termios::Termios;
+use anyhow::Result;
 
-pub fn set_raw_mode(fd: i32) -> Result<(), nix::Error> {
+pub fn set_raw_mode(fd: i32) -> Result<()> {
     let mut termios = Termios::from_fd(fd)?;
     nix::sys::termios::cfmakeraw(&mut termios);
     nix::sys::termios::tcsetattr(fd, nix::sys::termios::SetArg::TCSANOW, &termios)?;
