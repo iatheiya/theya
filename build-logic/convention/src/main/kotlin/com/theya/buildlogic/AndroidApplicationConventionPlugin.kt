8d82a7d9d0f4a7c154cb38dd372143b487d760e0
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 15:09:00 +0100

    Create AndroidApplicationConventionPlugin.kt

---
+++ build-logic/convention/src/main/kotlin/com/theya/buildlogic/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,36 @@
+package com.theya.buildlogic
+
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                compileSdk = 35
+
+                defaultConfig {
+                    targetSdk = 35
+                    minSdk = 24
+                }
+
+                buildTypes {
+                    release {
+                        isMinifyEnabled = true
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
