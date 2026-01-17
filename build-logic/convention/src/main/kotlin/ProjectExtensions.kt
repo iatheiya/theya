2988221c0a9b8a1d033744fe1cded3776a8a261e
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 15:20:51 +0100

    Delete build-logic/convention/src/main/kotlin/ProjectExtensions.kt

--- build-logic/convention/src/main/kotlin/ProjectExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.theya.buildlogic
-
-import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
-
-val Project.libs: VersionCatalog
-    get() = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
