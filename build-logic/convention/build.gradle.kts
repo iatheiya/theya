c46f96d3b14c6dc9654c18bff530551de70ef9cd
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 13:54:42 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -8,5 +8,4 @@ dependencies {
     implementation("com.android.tools.build:gradle:8.13.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.57.2")
-    implementation("org.mozilla.rust-android-gradle:plugin:0.9.3")
 }
