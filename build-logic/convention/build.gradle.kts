02e6bed0180f6396a67ef827e10a00909d0f6673
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-17 14:33:01 +0100

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -4,8 +4,20 @@ plugins {
 
 group = "com.theya.buildlogic"
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation("com.android.tools.build:gradle:8.13.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.57.2")
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin") {
+            useVersion("2.3.0")
+        }
+    }
 }
