b50a61e7fb87b75ac51d6e378bf2a8d4d6c95ad4
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-18 01:13:04 +0100

    Update termios.rs

--- modules/system/engine-rs/theya-pty/src/termios.rs
+++ modules/system/engine-rs/theya-pty/src/termios.rs
@@ -1,9 +1,11 @@
-use nix::sys::termios::Termios;
+use nix::sys::termios::{self, SetArg};
 use anyhow::Result;
+use std::os::unix::io::BorrowedFd;
 
 pub fn set_raw_mode(fd: i32) -> Result<()> {
-    let mut termios = Termios::from_fd(fd)?;
-    nix::sys::termios::cfmakeraw(&mut termios);
-    nix::sys::termios::tcsetattr(fd, nix::sys::termios::SetArg::TCSANOW, &termios)?;
+    let fd_ref = unsafe { BorrowedFd::borrow_raw(fd) };
+    let mut termios = termios::tcgetattr(fd_ref)?;
+    termios::cfmakeraw(&mut termios);
+    termios::tcsetattr(fd_ref, SetArg::TCSANOW, &termios)?;
     Ok(())
 }
