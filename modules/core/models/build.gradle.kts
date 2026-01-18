e461a7ec7bcb2edf62ac72fd57e7e134cdeeeaf8
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-18 20:25:43 +0100

    Update build.gradle.kts

--- modules/core/models/build.gradle.kts
+++ modules/core/models/build.gradle.kts
@@ -15,11 +15,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+}
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
-        }
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
 
