dcad46c4ad745ab5f0aad9f9d997fafbaa3dcc9c
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-18 02:19:06 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
 pluginManagement {
-    includeBuild("build-logic")
     repositories {
         gradlePluginPortal()
         mavenCentral()
@@ -8,10 +9,30 @@ pluginManagement {
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         google()
     }
+    resolutionStrategy {
+        eachPlugin {
+            val libs = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+            when (requested.id.id) {
+                "com.android.application" -> useModule("com.android.tools.build:gradle:${libs.findVersion("agp").get().requiredVersion}")
+                "com.android.library" -> useModule("com.android.tools.build:gradle:${libs.findVersion("agp").get().requiredVersion}")
+                "org.jetbrains.kotlin.android" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.findVersion("kotlin").get().requiredVersion}")
+                "org.jetbrains.kotlin.jvm" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.findVersion("kotlin").get().requiredVersion}")
+                "org.jetbrains.kotlin.kapt" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.findVersion("kotlin").get().requiredVersion}")
+                "org.jetbrains.kotlin.plugin.compose" -> useModule("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${libs.findVersion("kotlin").get().requiredVersion}")
+                "com.google.dagger.hilt.android" -> useModule("com.google.dagger:hilt-android-gradle-plugin:${libs.findVersion("hilt").get().requiredVersion}")
+                "io.github.MatrixDev.android-rust" -> useModule("io.github.MatrixDev:android-rust:${libs.findVersion("rustAndroid").get().requiredVersion}")
+            }
+        }
+    }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
     repositories {
         mavenCentral()
         gradlePluginPortal()
