540556f575789144954bb51f4e5ed21800150c74
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-18 03:57:13 +0100

    Update AndroidHiltConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/com/theya/buildlogic/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/theya/buildlogic/AndroidHiltConventionPlugin.kt
@@ -2,19 +2,33 @@ package com.theya.buildlogic
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            val catalogs = extensions.findByType(VersionCatalogsExtension::class.java)
+            val libs = try {
+                catalogs?.named("libs")
+            } catch (_: Exception) { null }
+
+            val hiltVersion = libs?.findVersion("hilt")?.orElse(null)?.requiredVersion ?: "2.58.0"
+
+            val hiltAndroidProvider = libs?.findLibrary("hilt-android")?.orElse(null)
+            val hiltCompilerProvider = libs?.findLibrary("hilt-compiler")?.orElse(null)
+
             with(pluginManager) {
                 apply("com.google.dagger.hilt.android")
                 apply("org.jetbrains.kotlin.kapt")
             }
 
             dependencies {
-                add("implementation", libs.findLibrary("hilt.android").get())
-                add("kapt", libs.findLibrary("hilt.compiler").get())
+                val impl = hiltAndroidProvider ?: project.dependencies.create("com.google.dagger:hilt-android:$hiltVersion")
+                val kaptDep = hiltCompilerProvider ?: project.dependencies.create("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+                add("implementation", impl)
+                add("kapt", kaptDep)
             }
         }
     }
